2226436a5ff3ab0c263a8ee5f81d4ccb89365d2b
Sangeon Park <pse961202@gmail.com>
2016-03-02 05:05:08 +0900

    Fixed dirty codes Validity unchecked. Please use at your own risk.

--- source/GamePlay.py
+++ source/GamePlay.py
@@ -24,199 +24,32 @@ class GamePlay(GraphMaking): # Inheritance
     # FindCombination(DepartureNum,Matrix)[1] return Reflection Number
     # default Matrix is self.Matrix
         if Matrix == None:
-           Matrix = self.Random_Matrix
+            Matrix = self.Random_Matrix
         n = 0
-        if DepartureNum == 1:
-            x = 0
-            y = 1
-            direct = 'd'    
-        elif DepartureNum == 2:
-            x = 0
-            y = 2
-            direct = 'd' 
-        elif DepartureNum == 3:
-            x = 0
-            y = 3
-            direct = 'd' 
-        elif DepartureNum == 4:
-            x = 0
-            y = 4
-            direct = 'd' 
-        elif DepartureNum == 5:
-            x = 0
-            y = 5
-            direct = 'd' 
-        elif DepartureNum == 6:
-            x = 1
-            y = 6
-            direct = 'l' 
-        elif DepartureNum == 7:
-            x = 2
-            y = 6
-            direct = 'l'
-        elif DepartureNum == 8:
-            x = 3
-            y = 6
-            direct = 'l'
-        elif DepartureNum == 9:
-            x = 4
-            y = 6
-            direct = 'l'
-        elif DepartureNum == 10:
-            x = 5
-            y = 6
-            direct = 'l'
-        elif DepartureNum == 11:
-            x = 6
-            y = 5
-            direct = 'u'
-        elif DepartureNum == 12:
-            x = 6
-            y = 4
-            direct = 'u'
-        elif DepartureNum == 13:
-            x = 6
-            y = 3
-            direct = 'u'
-        elif DepartureNum == 14:
-            x = 6
-            y = 2
-            direct = 'u'
-        elif DepartureNum == 15:
-            x = 6
-            y = 1
-            direct = 'u'
-        elif DepartureNum == 16:
-            x = 5
-            y = 0
-            direct = 'r'
-        elif DepartureNum == 17:
-            x = 4
-            y = 0
-            direct = 'r'
-        elif DepartureNum == 18:
-            x = 3
-            y = 0
-            direct = 'r'
-        elif DepartureNum == 19:
-            x = 2
-            y = 0
-            direct = 'r'
-        elif DepartureNum == 20:
-            x = 1
-            y = 0
-            direct = 'r'
-        while True:
-            if direct == 'd':
-                if Matrix[x][y] == 0:
-                    x = x + 1
-                elif Matrix[x][y] == 1:
-                    y = y - 1
-                    n = n + 1
-                    direct = 'l'
-                elif Matrix[x][y] == 2:
-                    y = y + 1
-                    n = n + 1
-                    direct = 'r'
-                    
-            elif direct == 'u':
-                if Matrix[x][y] == 0:
-                    x = x - 1
-                elif Matrix[x][y] == 1:
-                    y = y + 1
-                    n = n + 1
-                    direct = 'r'
-                elif Matrix[x][y] == 2:
-                    y = y - 1
-                    n = n + 1
-                    direct = 'l'
-                    
-            elif direct == 'l':
-                if Matrix[x][y] == 0:
-                    y = y - 1
-                elif Matrix[x][y] == 1:
-                    x = x + 1
-                    n = n + 1
-                    direct = 'd'
-                elif Matrix[x][y] == 2:
-                    x = x - 1
-                    n = n + 1
-                    direct = 'u'
-                    
-            elif direct == 'r':
-                if Matrix[x][y] == 0:
-                    y = y + 1
-                elif Matrix[x][y] == 1:
-                    x = x - 1
-                    n = n + 1
-                    direct = 'u'
-                elif Matrix[x][y] == 2:
-                    x = x + 1
-                    n = n + 1
-                    direct = 'd'
 
-            
-                
-            if x == 0 and y == 1:
-                result = 1
+        xlist = [0]*5 + range(1,6) + [6]*6 + range(1,6)[::-1]
+        ylist = xlist[5:] + xlist[:5]
+        xylist = zip(xlist,ylist)
+
+        direct = [[1,0],[0,-1],[-1,0],[0,1]][(DepartureNum - 1)/5]
+        x = xlist[DepartureNum - 1]
+        y = ylist[DepartureNum - 1]
+
+        while True:
+            curr = Matrix[x][y]
+            if curr == 0:
+                x += direct[0]
+            else:
+                n += 1
+                a,b = direct
+                if curr == 2:
+                    direct = b, a
+                else:
+                    direct = -b, -a
+            if xylist.count((x,y)):
+                result = xylist.index((x,y))
                 break
-            elif x == 0 and y == 2:
-                result = 2
-                break
-            elif x == 0 and y == 3:
-                result = 3
-                break
-            elif x == 0 and y == 4:
-                result = 4
-                break
-            elif x == 0 and y == 5:
-                result = 5
-                break
-            elif x == 1 and y == 6:
-                result = 6
-                break
-            elif x == 2 and y == 6:
-                result = 7
-                break
-            elif x == 3 and y == 6:
-                result = 8
-                break
-            elif x == 4 and y == 6:
-                result = 9
-                break
-            elif x == 5 and y == 6:
-                result = 10
-                break
-            elif x == 6 and y == 5:
-                result = 11
-                break
-            elif x == 6 and y == 4:
-                result = 12
-                break
-            elif x == 6 and y == 3:
-                result = 13
-                break
-            elif x == 6 and y == 2:
-                result = 14
-                break
-            elif x == 6 and y == 1:
-                result = 15
-                break
-            elif x == 5 and y == 0:
-                result = 16
-                break
-            elif x == 4 and y == 0:
-                result = 17
-                break
-            elif x == 3 and y == 0:
-                result = 18
-                break
-            elif x == 2 and y == 0:
-                result = 19
-                break
-            elif x == 1 and y == 0:
-                result = 20
-                break
+
         return [result,n]
 
     def NewGame(self):
@@ -226,3 +59,4 @@ class GamePlay(GraphMaking): # Inheritance
         print "NewGame : Show Manual"
         print "1. Find Combination -> Command \"FindCombination(DepartureNum)\""
         print "2. Check Graph -> Command \"CheckGraph()\""
+
--- source/GraphMaking.py
+++ source/GraphMaking.py
@@ -5,11 +5,10 @@
 """
 
 from Debug import * # debug Mode
-import random
+from random import randrange as rr, shuffle
 
 class GraphMaking(DebugMatrix):
     def MakeGraph(self, RandomGraph = True,UserTable = True):
-        # if RandomGraph == True Make Random Graph-------------------
         if RandomGraph:
             self.Random_Matrix = [
                 [0,0,0,0,0,0,0],
@@ -20,42 +19,32 @@ class GraphMaking(DebugMatrix):
                 [0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0]
                 ]
-            for i in range(2):
-                if(random.randrange(0,2) == 1):
-                    while True:
-                        x = random.randrange(1,5)
-                        y = random.randrange(1,5)
-                        if self.Random_Matrix[x][y] == 0 and self.Random_Matrix[x][y+1] == 0:
-                            break
-                    self.Random_Matrix[x][y] = random.randrange(1,3)
-                    if self.Random_Matrix[x][y] == 1:
-                        self.Random_Matrix[x][y+1] = 2
-                    else:
-                        self.Random_Matrix[x][y+1] = 1
-                else:
-                    while True:
-                        x = random.randrange(1,5)
-                        y = random.randrange(1,5)
-                        if self.Random_Matrix[x][y] == 0 and self.Random_Matrix[x+1][y] == 0:
-                            break
-                    self.Random_Matrix[x][y] = random.randrange(1,3)
-                    if self.Random_Matrix[x][y] == 1:
-                        self.Random_Matrix[x+1][y] = 2
-                    else:
-                        self.Random_Matrix[x+1][y] = 1
-            for i in range(2):
+            for _ in range(2):
+		dir = [0,1]
+		dir.shuffle()
                 while True:
-                    x = random.randrange(1,5)
-                    y = random.randrange(1,5)
-                    if self.Random_Matrix[x][y] == 0 and self.Random_Matrix[x][y+1] == 0 and self.Random_Matrix[x+1][y] == 0 and self.Random_Matrix[x+1][y+1] == 0:
+                    x = rr(1,5)
+                    y = rr(1,5)
+                    if self.Random_Matrix[x][y] == self.Random_Matrix[x+dir[0]][y+dir[1]] == 0:
                         break
-                self.Random_Matrix[x][y] = 1
-                self.Random_Matrix[x][y+1] = 2
-                self.Random_Matrix[x+1][y] = 2
-                self.Random_Matrix[x+1][y+1] = 1
-        # if RandomGraph == True Make Random Graph END---------------
+                self.Random_Matrix[x][y] = rr(1,3)
+                if self.Random_Matrix[x][y] == 1:
+                    self.Random_Matrix[x+dir[0]][y+dir[1]] = 2
+                else:
+                    self.Random_Matrix[x+dir[0]][y+dir[1]] = 1
 
-        # if UserTable == True Make User_Table-----------------------
+            for _ in range(2):
+                while True:
+                    x = rr(1,5)
+                    y = rr(1,5)
+                    if self.Random_Matrix[x][y] == self.Random_Matrix[x][y+1] == \
+		    self.Random_Matrix[x+1][y] == self.Random_Matrix[x+1][y+1] == 0:
+                        break
+                self.Random_Matrix[x][y] = self.Random_Matrix[x+1][y+1] = 1
+                self.Random_Matrix[x][y+1] = self.Random_Matrix[x+1][y] = 2
+
+
+       
         self.User_Matrix=[
             [0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0],
@@ -65,6 +54,7 @@ class GraphMaking(DebugMatrix):
             [0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0]
             ]
+
         # if UserTable == True Make User_Table END-------------------
     def RemoveGraph(self, RandomGraph = True,UserGraph = True):
         # if RandomGraph == True: User Graph - > User Table----------
